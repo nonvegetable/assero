20b577421ca1fc2cc6f1215fe23732be99388275
nonvegetable <100308933+nonvegetable@users.noreply.github.com>
2025-02-10 03:51:04 +0530

    removed imports

--- assero-frontend/src/app/page.tsx
+++ assero-frontend/src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import HomePage from "../pages/homepage";
-import CreateAsset from "../pages/create_asset";
-import Link from "next/link";
 
 export default function Page() {
   return (
